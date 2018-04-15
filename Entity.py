b83ea0fa15e51b1139002ee02ba55c070782b524
mT9b6oYZ <34481029+mT9b6oYZ@users.noreply.github.com>
2018-04-15 17:49:51 +0200

    Add files via upload

--- Entity.py
+++ Entity.py
@@ -1,1 +1,52 @@
+class Entity:
+	temp = 0
+	entities = []
 
+	def __init__(x, y, image, nbAnimsFrames, pace):
+		self.sprite = pygame.image.load(image)
+		self.position = self.sprite.get_rect()
+		self.size = self.position.size
+		self.position = self.position.move(x, y)
+		self.anim = 0
+		self.frame = 0
+		self.maxAnimsFrames = 0
+		for i in nbAnimsFrames:
+			if i >= self.maxAnimsFrames:
+				self.maxAnimsFrames = i
+		self.nbAnimsFrames = nbAnimsFrames
+		self.pace = pace
+		entities.append(self)
+
+	def draw(window):
+		@staticmethod
+		for i in entities:
+			entities[i].render(window)
+		temp = (temp + 1) % 256
+
+	def render(window):
+		if anim < 4:
+			window.blit(sprite, position, (0, anim * size[1] / len(nbAnimsFrames), size[0] / maxAnimsFrames, size[1] / len(nbAnimsFrames)))
+		if anim >= 4 and anim < 8:
+			if temp % pace == 0:
+				frame = (frame + 1) % nbAnimsFrames[i]
+			window.blit(sprite, position, (frame * size[0] / maxAnimsFrames, anim * size[1] / len(nbAnimsFrames), size[0] / maxAnimsFrames, size[1] / len(nbAnimsFrames)))
+			if anim == 0:
+				position = position.move(0, 1)
+			if anim == 1:
+				position = position.move(-1, 0)
+			if anim == 2:
+				position = position.move(0, -1)
+			if anim == 3:
+				position = position.move(1, 0)
+		if anim >= 8:
+			if temp % pace == 0:
+				frame += 1
+			if frame == nbAnimsFrames[anim]:
+				frame = 0
+				anim %= 4
+			window.blit(sprite, position, (frame * size[0] / maxAnimsFrames, anim * size[1] / len(nbAnimsFrames), size[0] / maxAnimsFrames, size[1] / len(nbAnimsFrames)))
+
+	def unwalk():
+		if anim >= 4 and anim < 8:
+			anim %= 4
+			frame = 0
