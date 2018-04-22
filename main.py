3cec598d304217bcef17b72620d89962653813ae
Gaël <38374449+Logharaa@users.noreply.github.com>
2018-04-22 17:11:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,33 @@
-import pygame, math, copy
+import pygame, math, copy, sys
 from pygame.locals import *
 from Map import *
 from Entity import Entity
-
+sys.path.insert(0, "Entity")
+import Player
+ 
 pygame.mixer.pre_init(44100, 16, 2, 4096)
 pygame.init()
 pygame.mixer.init()
 pygame.display.set_caption("Kill the Anthony 2")
 
 window = pygame.display.set_mode((1440, 900))
+
+map1 = Map('textures/tmx/level1.tmx')
+map_img = map1.make_map()
+
+player = Player.Player(0, 0, "textures/link.png", [1, 1, 1, 1, 10, 10, 10, 10], 16)
+
 clock = pygame.time.Clock()
 
 windowOpen = True
 while windowOpen:
+
 	for event in pygame.event.get():
 		if event.type == QUIT:
 			exit()
-	
-	map1 = Map('textures/tmx/level1.tmx')
-	map_img = map1.make_map()
+
 	window.blit(map_img, (0, 0))
-
-	Entity.Entity.draw(window)
-
+	Entity.draw(window)
 	pygame.display.flip()
+
+	clock.tick(144)
