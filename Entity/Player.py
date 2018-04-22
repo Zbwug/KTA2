8196b3982f1c1eb11a9fc4bd02b8314d25c47ef9
Gaël <38374449+Logharaa@users.noreply.github.com>
2018-04-22 17:12:25 +0200

    Update Player.py

--- Entity/Player.py
+++ Entity/Player.py
@@ -1,16 +1,25 @@
+import pygame, sys
+from pygame.locals import *
 from Entity import Entity
+
 class Player(Entity):
 	def __init__(self, x, y, image, nbAnimsFrames, pace):
 		super(Player, self).__init__(x, y, image, nbAnimsFrames, pace)
 
-	def render(window):
+	def render(self, window):
 		super(Player, self).render(window)
 		key = pygame.key.get_pressed()
-		if key[pygame.K_s]:
-			anim = 0
-		if key[pygame.K_q]:
-			anim = 1
-		if key[pygame.K_z]:
-			anim = 2
-		if key[pygame.K_d]:
-			anim = 3
+		
+		prevX = self.position.x
+		prevY = self.position.y
+
+		if key[pygame.K_DOWN]:
+			self.anim = 4
+		elif key[pygame.K_LEFT]:
+			self.anim = 5
+		elif key[pygame.K_UP]:
+			self.anim = 6
+		elif key[pygame.K_RIGHT]:
+			self.anim = 7
+		else:
+			self.unwalk()
