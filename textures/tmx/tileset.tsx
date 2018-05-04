21176110f550c13e9d11d079ba35a6ddc9bf433a
Gaël <38374449+Logharaa@users.noreply.github.com>
2018-05-04 17:08:55 +0200

    Add files via upload

---
+++ textures/tmx/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="map" tilewidth="16" tileheight="16" tilecount="65536" columns="256">
+ <image source="map.png" width="4096" height="4096"/>
+</tileset>
