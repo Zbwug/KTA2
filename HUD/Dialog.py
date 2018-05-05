1b328fdf16fffb1058a8dabb38492f353bcf6c25
Skilldus <34102919+Skilldus@users.noreply.github.com>
2018-05-05 15:52:10 +0200

    Dialog (textbox object)

--- HUD/Dialog.py
+++ HUD/Dialog.py
@@ -1,1 +1,14 @@
+import pygame, sys
 
+class Dialog:
+	
+	def __init__(self, x, txt):
+		self.x = x
+		self.textbox = pygame.image.load("textures/hud/textbox.png")
+		self.textbox = pygame.transform.scale(self.textbox, (1020, 200))
+		self.fontDialog = pygame.font.Font(None,30)
+		self.txt = self.fontDialog.render(txt, 1, (0, 0, 0))
+		
+	def box(self, window):
+		window.blit(self.textbox, (0, 568))
+		window.blit(self.txt, (self.x, 768-(200/2)))
