02dddb306a66700b633703462b53d9bdb08a4914
Skilldus <34102919+Skilldus@users.noreply.github.com>
2018-05-09 21:01:52 +0200

    Son

--- HUD/Dialog.py
+++ HUD/Dialog.py
@@ -7,9 +7,13 @@ class Dialog:
 		self.textbox = pygame.transform.scale(self.textbox, (1020, 200))
 		self.fontDialog = pygame.font.Font(None,30)
 		self.txt = self.fontDialog.render(txt, 1, (0, 0, 0))
-		self.popsound = pygame.mixer.Sound("textures/hud/pop.wav")
 
+
+		
 	def box(self, window):
 		window.blit(self.textbox, (0, 568))
 		window.blit(self.txt, (self.x, 768-(200/2)))
-		self.popsound.play()
+
+        
+
+		
