cfe739639bcd45069b618cdbc2545a1b55052239
Skilldus <34102919+Skilldus@users.noreply.github.com>
2018-05-10 23:50:06 +0200

    Dialog new methods

--- HUD/Dialog.py
+++ HUD/Dialog.py
@@ -1,6 +1,8 @@
-import pygame, sys
+import pygame, sys, linecache
 
 class Dialog:
+	
+	
 	def __init__(self, x, txt):
 		self.x = x
 		self.textbox = pygame.image.load("textures/hud/textbox.png")
@@ -8,9 +10,24 @@ class Dialog:
 		self.fontDialog = pygame.font.Font(None,30)
 		self.txt = self.fontDialog.render(txt, 1, (0, 0, 0))
 
+
+		
 	def box(self, window):
 		window.blit(self.textbox, (0, 568))
 		window.blit(self.txt, (self.x, 768-(200/2)))
+		
+	def getNumberOfLines():
+		n = 0
+		script = open("textures/hud/script.txt", "r")
+		for file in script:
+			n += 1
+		return n
+	
+	def getLine(n):
+		script = "textures/hud/script.txt"
+		return linecache.getline(script, n)
+		
+		
 
         
 
