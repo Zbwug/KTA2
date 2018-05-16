76caabbaa34eb5b301d419ec7b3231c3a667edd6
Skilldus <34102919+Skilldus@users.noreply.github.com>
2018-05-16 16:06:20 +0200

    New Dialog Object & features

--- HUD/Dialog.py
+++ HUD/Dialog.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
 import pygame, sys, linecache
 
 class Dialog:
@@ -8,20 +10,34 @@ class Dialog:
 		self.textbox = pygame.transform.scale(self.textbox, (1020, 200))
 		self.fontDialog = pygame.font.Font(None,30)
 		self.script = script
-		self.txt = self.fontDialog.render(self.getLine(dCount).rstrip("\n"), 1, (0, 0, 0))
+		self.txt = self.fontDialog.render(self.getLine(dCount), 1, (0, 0, 0))
 
 
 	def box(self, window):
 		window.blit(self.textbox, (0, 568))
 		window.blit(self.txt, (self.x, 768-(200/2)))
 
-	def getNumberOfLines(self):
-		script = open(self.script, "r")
-		n = 0
-		for lines in script:
-			n += 1
-		return n
-
 	def getLine(self, n):
-		return linecache.getline(self.script, n)
+		script = open(self.script, "r")
+		str = script.read()
+		count = 0
+		result = ""
+		chr = "\n"
+		for i in range(len(str)):
+			if str[i] == chr:
+				count += 1
+			if count == n and str[i] != chr:
+				result += str[i]
+			if count - 1 == n:
+				return result
+				
+	def maxLines(self):
+		script = open(self.script, "r")
+		str = script.read()
+		count = 0
+		chr = "\n"
+		for i in range(len(str)):
+			if str[i] == chr:
+				count += 1
+		return count + 1
 		
