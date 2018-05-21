01782e89f5295c3b508b443dadb4aa85038569ad
mT9b6oYZ <34481029+mT9b6oYZ@users.noreply.github.com>
2018-05-21 21:07:21 +0200

    Add files via upload

--- Entity/Human/Enemy.py
+++ Entity/Human/Enemy.py
@@ -1,1 +1,21 @@
+import sys
+sys.path.insert(0, "..")
+from Human import Human
+
+class Enemy(Human):
+	def __init__(self, x, y, image, nbAnimsFrames, pace, player, pattern):
+		super(Enemy, self).__init__(x, y, image, nbAnimsFrames, pace, player, pattern)
+		self.pursuit = False
+
+	def setTarget(self):
+		if self.pursuit:
+			self.target = [self.player.position.x, self.player.position.y]
+			if self.hasLost([self.player.position.x, self.player.position.y]):
+				self.pursuit = False
+				self.patternstate = 0
+				self.gotoPattern()
+		else:
+			self.gotoPattern()
+			if self.hasNoticed([self.player.position.x, self.player.position.y]):
+				self.pursuit = True
 
