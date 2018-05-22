8a54dc255dab0bdb20027fcf60014bc644bb2eab
Gaël <38374449+Logharaa@users.noreply.github.com>
2018-05-22 13:49:53 +0200

    Add files via upload

---
+++ textures/tmx/woods.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="woods" tilewidth="16" tileheight="16" tilecount="4096" columns="64">
+ <image source="woods.png" width="1024" height="1024"/>
+</tileset>
