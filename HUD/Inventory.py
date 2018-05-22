7ee1e02d4a3a0d1633ff73312a07e2558428fbf2
Skilldus <34102919+Skilldus@users.noreply.github.com>
2018-05-22 15:09:30 +0200

    Inventory changes

--- HUD/Inventory.py
+++ HUD/Inventory.py
@@ -9,18 +9,32 @@ class Inventory:
 		self.y = y
 		self.img = pygame.image.load("textures/hud/inventory.png")
 		self.img = pygame.transform.scale(self.img, (int(148*1.5), int(39*1.5)))
+		self.listOfItems = items[]
 		self.fontDialog = pygame.font.Font(None,30)
 		self.full = self.fontDialog.render("L'inventaire est plein !", 1, (255, 0, 0))
+		self.full = self.fontDialog.render("L'inventaire est vide !", 1, (0, 255, 0))
+		
 		
 	def box(self, window):
-		window.blit(self.img, (self.x, self.y))
+		window.blit(self.img, (self.x, self.y))	
 		
 	def addItem(self, window, itemId):
-		items = []
-		if len(items) < 0:
+		if len(self.listOfItems) < 0:
 			return null
-		if len(items) >= 4:
+		if len(self.listOfItems) >= 4:
 			window.blit(self.full, (self.x, self.y - int(39*1.4)))
+		else:
+			#TODO
+		
+	def removeItem(self, window, itemId):
+		if len(self.listOfItems) <= 0:
+			window.blit(self.empty, (self.x, self.y - int(39*1.4)))
+		if len(self.listOfItems) > 4:
+			return null
+		if len(self.listOfItems) <= 4:
+			#TODO
+			
+	def showItems(self, )
 		
 			
 		
