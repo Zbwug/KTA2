81589d89140dd1134b8b7f0abf72e02e67452a10
Skilldus <34102919+Skilldus@users.noreply.github.com>
2018-05-23 21:42:59 +0200

    potentiometer

--- Menu/Potentiometer.py
+++ Menu/Potentiometer.py
@@ -1,1 +1,22 @@
+import pygame, sys
 
+class Potentiometer:
+
+	def __init__(self, x, y, w, h, color, id):
+		self.x = x
+		self.y = y
+		self.w = w
+		self.h = h
+		self.rect = pygame.Rect((self.x, self.y, self.w, self.h))
+		self.color = color
+		self.id = id
+
+	def draw(self, window):
+		pygame.draw.rect(window, self.color, self.rect)
+
+	def deplacement(self, window, power):
+		key = pygame.key.get_pressed()
+		if key[pygame.K_LEFT]:
+			self.rect = self.rect.move(-power, 0)
+		if key[pygame.K_RIGHT]:
+			self.rect = self.rect.move(power, 0)
