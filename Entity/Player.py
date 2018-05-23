f854630e6f963e72cb23afdb6f854fa96e91519d
Gaël <38374449+Logharaa@users.noreply.github.com>
2018-05-23 17:08:50 +0200

    Update Player.py

--- Entity/Player.py
+++ Entity/Player.py
@@ -9,13 +9,13 @@ class Player(Entity):
 	def render(self, window, camera):
 		super(Player, self).render(window, camera)
 		key = pygame.key.get_pressed()
-		if key[pygame.K_DOWN]:
+		if key[pygame.K_DOWN] and Entity.mapmatrix[int((self.position.y + 4) / 16 + 1)][int((self.position.x + 0) / 16)] != 1 and Entity.mapmatrix[int((self.position.y + 4) / 16 + 1)][int((self.position.x + 0) / 16 + 1)] != 1:
 			self.anim = 4
-		elif key[pygame.K_LEFT]:
+		elif key[pygame.K_LEFT] and Entity.mapmatrix[int((self.position.y + 0) / 16)][int((self.position.x - 4) / 16)] != 1 and Entity.mapmatrix[int((self.position.y + 0) / 16 + 1)][int((self.position.x - 4) / 16)] != 1:
 			self.anim = 5
-		elif key[pygame.K_UP]:
+		elif key[pygame.K_UP] and Entity.mapmatrix[int((self.position.y - 4) / 16)][int((self.position.x + 0) / 16)] != 1 and Entity.mapmatrix[int((self.position.y - 4) / 16)][int((self.position.x + 0) / 16 + 1)] != 1:
 			self.anim = 6
-		elif key[pygame.K_RIGHT]:
+		elif key[pygame.K_RIGHT] and Entity.mapmatrix[int((self.position.y + 0) / 16)][int((self.position.x + 4) / 16 + 1)] != 1 and Entity.mapmatrix[int((self.position.y + 0) / 16 + 1)][int((self.position.x + 4) / 16 + 1)] != 1:
 			self.anim = 7
 		else:
 			self.unwalk()
