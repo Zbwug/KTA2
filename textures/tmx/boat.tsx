c72e9bd74dca1858f155dc7ab64f7dfb9cf6d41c
Gaël <38374449+Logharaa@users.noreply.github.com>
2018-05-26 22:48:33 +0200

    Add files via upload

---
+++ textures/tmx/boat.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="boat" tilewidth="16" tileheight="16" tilecount="90" columns="9">
+ <image source="boat.png" width="144" height="160"/>
+</tileset>
---
+++ textures/tmx/darkworld.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="darkworld" tilewidth="16" tileheight="16" tilecount="65536" columns="256">
+ <image source="darkworld.png" width="4096" height="4096"/>
+</tileset>
