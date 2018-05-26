0685cd1047303d40fa5cd55d037a989a0f926477
Skilldus <34102919+Skilldus@users.noreply.github.com>
2018-05-26 15:52:25 +0200

    changes

--- Menu/Potentiometer.py
+++ Menu/Potentiometer.py
@@ -2,21 +2,25 @@ import pygame, sys
 
 class Potentiometer:
 
-	def __init__(self, x, y, w, h, color, id):
+	def __init__(self, x, y, w, h, texture, form):
 		self.x = x
 		self.y = y
 		self.w = w
 		self.h = h
-		self.rect = pygame.Rect((self.x, self.y, self.w, self.h))
-		self.color = color
-		self.id = id
+		self.texture = pygame.image.load(texture)
+		self.texture = pygame.transform.scale(self.texture, (int(self.w), int(self.h)))
+		self.texturepos = self.texture.get_rect()
+		self.form = form
 
 	def draw(self, window):
-		pygame.draw.rect(window, self.color, self.rect)
+		if self.form == "Rectangle":
+			window.blit(self.texture, self.texturepos, (-self.x, -self.y, 1024, 768))
+		if self.form == "Circle":
+			pygame.draw.circle(window, pygame.Color(255, 255, 0), (int(self.x), int(self.y) + 8), self.h -5)
 
 	def deplacement(self, window, power):
 		key = pygame.key.get_pressed()
 		if key[pygame.K_LEFT]:
-			self.rect = self.rect.move(-power, 0)
+			self.texturepos = self.texturepos.move(-power, 0)
 		if key[pygame.K_RIGHT]:
-			self.rect = self.rect.move(power, 0)
+			self.texturepos = self.texturepos.move(power, 0)
